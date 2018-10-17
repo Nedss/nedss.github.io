74150ea842255f257856c728652fa4381c69d230
Nedss <nicolas@soirat.com>
2018-10-17 09:11:34 +0200

    setting url

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Nedss'
 SITENAME = "Nedss en Vrac !"
-SITEURL = 'https://nedss.github.io/'
+SITEURL = 'https://nedss.github.io/index.html'
 THEME = 'pelican-themes/pure-single'
 PATH = 'content'
 
